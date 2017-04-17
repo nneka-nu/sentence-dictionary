534e14bfdc1f873a8a8c29fb65bc722156c8b327
Nneka <nneka.dev@gmail.com>
2017-04-17 14:09:04 -0700

    remove react-minification warning

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = [
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendors'
       }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
       new webpack.optimize.UglifyJsPlugin({
         sourceMap: true
       })
